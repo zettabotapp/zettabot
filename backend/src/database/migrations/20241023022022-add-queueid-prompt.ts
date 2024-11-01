0c0d9de418f6a952f0052f7918a789e5f7438434
Arthur Souza Armond <asarmond82@gmail.com>
2024-11-01 17:37:56 -0300

    [ fix ]: Error in prompt QueueID

---
+++ backend/src/database/migrations/20241023022022-add-queueid-prompt.ts
@@ -0,0 +1,16 @@
+import { QueryInterface, DataTypes } from "sequelize";
+
+module.exports = {
+    up: async(queryInterface : QueryInterface) => {
+        await queryInterface.addColumn('Prompts', 'queueId', {
+            type: DataTypes.INTEGER,
+            references: { model: "Queues", key: "id" },
+            onUpdate: "NO ACTION",
+            onDelete: "NO ACTION",
+            allowNull: false
+          })
+    },
+    down: async(queryInterface: QueryInterface) => {
+        await queryInterface.removeColumn('Prompts', 'queueId');
+    }
+}
